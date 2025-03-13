551deb9d99af9f36d26d81944ce53f4805808477
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-03-13 14:34:26 +0200

    Lift email state and pass to comment create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
             <Routes>
                 <Route index element={<Home/>} />
                 <Route path='/games' element={<GameCatalog/>} />
-                <Route path='/games/:gameId/details' element={<GameDetails />}/>
+                <Route path='/games/:gameId/details' element={<GameDetails email={email} />}/>
                 <Route path='/games/create' element={<GameCreate/>} />
                 <Route path='/games/:gameId/edit' element={<GameEdit/>}/>
                 <Route path='/login' element={<Login onLogin={useLoginHandler}/>} />
